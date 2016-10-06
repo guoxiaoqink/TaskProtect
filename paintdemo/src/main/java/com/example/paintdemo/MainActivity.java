82ebdd17b301155b8c25afad3243e49f5946c55d
guoxiaoqin <1037930134@qq.com>
2016-10-06 14:56:14 +0800

    添加的toast2

--- paintdemo/src/main/java/com/example/paintdemo/MainActivity.java
+++ paintdemo/src/main/java/com/example/paintdemo/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
     }
 
 }
